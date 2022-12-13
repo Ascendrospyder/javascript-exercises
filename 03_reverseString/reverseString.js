709ae7531eaabf89f75d50c05de7cfe009b341d9
Arindam Mukherjee <z5421641@ad.unsw.edu.au>
2022-12-13 21:14:20 +1100

    Added the functionality for the function and fixed the style

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function (string) {
+  // create an empty string to hold the result
+  let result = "";
 
+  // loop through the string, starting from the end to
+  // the start and add the elements to our result string
+  for (let i = string.length - 1; i >= 0; i--) {
+    result += string[i];
+  }
+  return result;
 };
 
 // Do not edit below this line
