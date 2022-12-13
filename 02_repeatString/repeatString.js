f0e019b78304c15212b5a8c6df9ff1073636cb0f
Arindam Mukherjee <z5421641@ad.unsw.edu.au>
2022-12-13 21:05:02 +1100

    Added the code for repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+  // create an empty array to hold to resulting string
+  let resultString = [];
 
+  // loop through the array num times and add string to the
+  // array
+  for (let i = 0; i < num; i++) {
+    resultString += string;
+  }
+  return resultString;
 };
 
 // Do not edit below this line
